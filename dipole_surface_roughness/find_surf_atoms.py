3001af7c74dcf63cf92f645df0cb43b9d809f1d8
scmartin <sethcmart@gmail.com>
2022-09-18 13:55:44 -0500

    Add mu and sigma to argparse. Add comments.

--- dipole_surface_roughness/find_surf_atoms.py
+++ dipole_surface_roughness/find_surf_atoms.py
@@ -8,37 +8,43 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser(description="Analyze a LAMMPS snapshot to find surface atoms.")
 parser.add_argument('-f','--file',type=str, required=True)
 parser.add_argument('-c','--cutoff',type=float, default=1000, help='cutoff value of the dipole magnitude to be a surface atom')
-parser.add_argument('-s','--sphere',type=float, default=6.0, help="sphere of neighbor list radius")
+parser.add_argument('-r','--radius',type=float, default=6.0, help="neighbor list radius")
+parser.add_argument('-m','--mu',type=float, default=0.0, help='center of the gaussian weighting function')
+parser.add_argument('-s','--sigma',type=float, default=2.5, help='standard deviation of gaussian weight function')
 
+# Read a lammps trajectory
 args = parser.parse_args()
+mu = args.mu
+sigma = args.sigma
 traj = lammps.lammpstraj.pandas_reader(fname=args.file)
 
+# Take the final frame from the lammps trajectory
 final = traj[0].index.levels[0][-1]
 box = traj[1][final]
 frame = traj[0].loc[final].copy()
+# Generate a Freud box from the lammps trajectory
 box = freud.box.Box(box[0,1] - box[0,0],
                     box[1,1] - box[1,0],
                     box[2,1] - box[2,0])
 
-
+# Generate neighborlists
 nlist = freud.locality.AABBQuery(box, frame.loc[:,['x','y','z']])
 query = nlist.query(nlist.points,dict(mode='ball',r_max=args.sphere))
-
-# rdf = freud.density.RDF(30,10)
-# rdf.compute(nlist)
-
-# fig, ax = plt.subplots()
-# ax.plot(rdf.bin_centers, rdf.rdf, '-k')
-# plt.show()
-
 newnlist = query.toNeighborList()
 
 def weight(points, mu, sigma):
+    """Generate gaussian weights for neighbors of a central point.
+    
+    Keyword arguments:
+    points -- cartesian coordinates of neighbors. Coordinate origin is located at the central point.
+    mu -- center of the gaussian weighting function. mu = 0 sets maximum weight at the central point.
+    sigma -- width of the gaussian weighting function.
+    """
     dist = np.sqrt(np.sum(points**2,axis=1))
     return np.tile(np.exp(-0.5*(dist-mu)**2/sigma**2), (3,1)).T
 
-mu, sigma = input("enter mu and sigma: ").split()
-mu, sigma = float(mu), float(sigma)
+# mu, sigma = input("enter mu and sigma: ").split()
+# mu, sigma = float(mu), float(sigma)
 
 dipoles = np.zeros(nlist.points.shape[0])
 zdipoles = np.zeros(nlist.points.shape[0])
@@ -53,10 +59,10 @@ for i in range(nlist.points.shape[0]):
     zdipoles[i] = np.dot([0,0,1],dipole)
     dipoles[i] = np.dot(dipole,dipole)
 
-frame.loc[slice(None),'dipole'] = dipoles
+frame.loc[slice(None),'dipole_magnitude'] = dipoles
 frame.loc[slice(None),'zdipole'] = zdipoles
 fig, ax = plt.subplots(2,1)
-frame.hist(column='dipole', bins=35, ax=ax[0])
+frame.hist(column='dipole_magnitude', bins=35, ax=ax[0])
 frame.hist(column='zdipole', bins=25, ax=ax[1])
 plt.show()
 
